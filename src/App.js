f88a2d972d1e63f545f1425a08e32763a0b4ce37
Kushal Chaudhari <chaudharikushalganesh@gmail.com>
2022-12-03 11:59:23 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom';
 import Gpay from "./pages/Gpay.js";
 import Navbar from "./pages/Navbar";
 import Landing from "./pages/Landing";
+import Footer from "./pages/Footer"
 export default function App(){
   return(
     <Router>
     <Routes>
       <Route exact path ='/payment' element={<><Navbar/><Gpay/></> }/>
-      <Route exact path='/' element={<><Navbar/> <Landing/></> }/>
+      <Route exact path='/' element={<><Navbar/> <Landing/><Footer/></> }/>
       </Routes>     
     </Router>
      
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,41 +1,44 @@
 export default function Example() {
     return (
         <div class=" p-4 bg-gradient-to-r from-purple-500 to-pink-500 flex justify-center shadow-xl gap-24">
+           {/* 1st cardy */}
             <div class="max-w-sm bg-white rounded-lg shadow-md">
                 <a href="#">
                     <img class="rounded-t-lg" src="https://drive.google.com/uc?export=view&id=1vD54gxIKsP9o-2tO3UxwTd46XzEUycn6" alt="" />
                 </a>
                 <div class="p-5">
                     <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">You will be a part of saving earth.</h5>
                     </a>
-                    <p class="mb-3 font-normal text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <p class="mb-3 font-normal text-gray-400">Funds will be for every problems like for old age home, poor children and also for planet like cleaning ocean and so on.</p>
                    
                 </div>
             </div>
 
+            {/* 2nd card */}
             <div class="max-w-sm bg-white rounded-lg shadow-md">
                 <a href="#">
-                    <img class="rounded-t-lg" src="https://drive.google.com/uc?export=view&id=1vD54gxIKsP9o-2tO3UxwTd46XzEUycn6" alt="" />
+                    <img class="rounded-t-lg " src="https://drive.google.com/uc?export=view&id=1iaHhYIaMfivsG1hBBNsio-SYbeFqf1cH" alt="" />
                 </a>
                 <div class="p-5">
                     <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Everyone will be loved and cared.</h5>
                     </a>
-                    <p class="mb-3 font-normal text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <p class="mb-3 font-normal text-gray-400">Here is the biggest pro. Everyone means children, old people also animals will be cared and loved.</p>
                    
                 </div>
             </div>
 
+            {/* #\3rd card */}
             <div class="max-w-sm bg-white rounded-lg shadow-md">
-                <a href="#">
-                    <img class="rounded-t-lg" src="https://drive.google.com/uc?export=view&id=1vD54gxIKsP9o-2tO3UxwTd46XzEUycn6" alt="" />
+                <a className="flex justify-center" href="#">
+                    <img class="rounded-t-lg h-56 " src="https://drive.google.com/uc?export=view&id=1Kl245-lCg8T-AstZWIzm-1ZUpzggarWM" alt="" />
                 </a>
                 <div class="p-5">
                     <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Yes, you can be VOLUNTEER.</h5>
                     </a>
-                    <p class="mb-3 font-normal text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <p class="mb-3 font-normal text-gray-400">You can work and experience volunteering with us because it gives you happiness by seeing happy faces.</p>
                     
                 </div>
             </div>
